f36f276938405ad5a6c5fa3908140d8b8a9d82e0
Breno Silva <brenosill@hotmail.com>
2025-07-29 15:22:22 -0300

    feat: Adicionado novas rotas

--- back/routes/index.routes.js
+++ back/routes/index.routes.js
@@ -1,9 +1,13 @@
 import {Router} from "express";
 import AuthRouter from "./auth.routes.js";
+import MessageRouter from "./message.routes.js";
+import UserRouter from "./user.router.js";
 
 const MainRouter = Router();
 
 MainRouter.use('/auth',AuthRouter);
+MainRouter.use('/user',UserRouter);
+MainRouter.use('/message',MessageRouter);
 
 
 export default MainRouter
