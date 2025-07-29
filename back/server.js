c9565e6fe85106542a1eebde7d5ebb4071f229c2
Breno Silva <brenosill@hotmail.com>
2025-07-29 15:22:59 -0300

    feat: Adicionado nova função na raiz do projeto

--- back/server.js
+++ back/server.js
@@ -2,13 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import MainRouter from './routes/index.routes.js';
 import { dbConnection } from './lib/db.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-
+app.use(cookieParser());
 app.use('/api', MainRouter)
 
 
