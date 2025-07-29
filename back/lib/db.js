52b0e45f0612e12b9ac13c9de53a66cfa0eadd73
Breno Silva <brenosill@hotmail.com>
2025-07-29 15:21:45 -0300

    chore: Adicionado espaçamento

--- back/lib/db.js
+++ back/lib/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
+
 export const dbConnection = async () => {
     try {
        const coon = await mongoose.connect(process.env.MONGO_URL,);
