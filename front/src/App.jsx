a15a158f8e10d54c5ed7c30557ccec20571e86f5
Breno Silva <brenosill@hotmail.com>
2025-07-30 01:01:11 -0300

    cleanup: Limpado codigo html e adicionado rotas

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Login from "./pages/login/login"
+import Cadastro from "./pages/cadastro/cadastro"
+import Home from "./pages/home/home"
+import { Routes, Route } from 'react-router-dom'
+import  { Toaster } from 'react-hot-toast';
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className="p-4 h-screen flex items-center justify-center">
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="/cadastro" element={<Cadastro />} />
+				<Route path="/home" element={<Home />} />
+			</Routes>
+			<Toaster />
+		
+		</div>
+		
+	)
 }
 
 export default App
