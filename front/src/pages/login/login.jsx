8220d9c1c58d708c9926ef26745ccce935cf70ad
Breno Silva <brenosill@hotmail.com>
2025-07-30 01:04:03 -0300

    feat: Criado página de login

---
+++ front/src/pages/login/login.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+
+const Login = () => {
+
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    })
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        
+        try {
+           
+            console.log('123')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    
+
+    return (
+        <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+            <div className='w-full flex flex-col gap-4 p-6 rounded-lg shadow-md bg-gray-600 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-20 border border-gray-100'> 
+                <h1 className='text-2xl font-bold text-white gap-2 text-center'>
+                    <span className='text-blue-500'>Chat</span>
+                    <span className='text-white'>App | Login</span>
+                </h1>
+                <form onSubmit={handleSubmit}>
+                    
+                    <div className='flex flex-col gap-2'>
+                        
+                        <input 
+                            type="text"
+                            placeholder='Email'
+                            className='border-2 border-gray-300 rounded-md p-2 bg-white'
+                            value={user.email}
+                            onChange={(e) => setUser({...user, email: e.target.value})}
+                        />
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                    
+                        <input 
+                            type="password"
+                            placeholder='Password'
+                            className='border-2 border-gray-300 rounded-md p-2 bg-white'
+                            value={user.password}
+                            onChange={(e) => setUser({...user, password: e.target.value})}
+                        />
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                        <a href="/cadastro" className='text-blue-500 text-end'>{"Don't have an account? Register"}</a>
+                        <button className='bg-blue-500 text-white p-2 rounded-md'>Login</button>
+                    </div>
+                </form>
+                
+            </div>
+        </div>
+
+        
+    )
+}
+
+export default Login
