769cc97593235843e78ca85547903b7b0ced480d
Breno Silva <brenosill@hotmail.com>
2025-07-30 01:00:00 -0300

    feat: Adicionado cors da aplicação back end

--- back/server.js
+++ back/server.js
@@ -3,11 +3,16 @@ import dotenv from 'dotenv';
 import MainRouter from './routes/index.routes.js';
 import { dbConnection } from './lib/db.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 const app = express();
 
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}))
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api', MainRouter)
