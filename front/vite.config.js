f779c1a8dbac7d6124583267b7ddd132658e040a
Breno Silva <brenosill@hotmail.com>
2025-07-30 01:04:19 -0300

    feat: Adicionado configuração

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    host: true,
+    open: true,
+    cors: true,
+    
+  },
+  
   plugins: [react()],
 })
