109e8716b466820c74b06f8f75431caafede7456
Breno Silva <brenosill@hotmail.com>
2025-07-30 00:59:47 -0300

    chore: Alterado endpoint da api

--- back/routes/auth.routes.js
+++ back/routes/auth.routes.js
@@ -3,7 +3,7 @@ import { Register, Auth, Logout } from '../controllers/auth.controller.js';
 
 const authRouter = Router();
 
-authRouter.post('/register', Register);
+authRouter.post('/signup', Register);
 authRouter.post('/login', Auth);
 authRouter.post('/logout', Logout);
 
