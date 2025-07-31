eca226c4f122035a47fc6719b769be8b88411a03
Breno Silva <brenosill@hotmail.com>
2025-07-31 01:59:05 -0300

    chore: Componente de mensagens

--- front/src/components/messages.jsx
+++ front/src/components/messages.jsx
@@ -1,15 +1,46 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Message from './message'
+import useGetMessages from '../hooks/useGetMessages'
+import MessageSkeleton from './messageSkeleton'
+import { useListenMessages } from '../hooks/useListenMessages'
 
 const Messages = () => {
-  return (
-    <div className='px-4py-2 overflow-y-auto'>
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-    </div>
-  )
+
+    const {loading, messages} = useGetMessages()
+	useListenMessages()
+
+	const lastMessageRef= useRef()
+
+    useEffect(() => {
+        setTimeout(() => {
+            lastMessageRef.current?.scrollIntoView({behavior: 'smooth'})
+        }, 100)
+    }, [messages])
+
+
+	return (
+		<div className='px-4 flex-1 overflow-auto'>
+
+			{!loading && messages.length > 0 && 
+				messages.map((message, index) => (
+					<Message key={message?._id || `message-${index}`} message={message} ref={lastMessageRef} />
+				))
+		
+			}
+
+
+			{loading && Array.from({length: 10}).map((_, index) => (
+				<MessageSkeleton key={index} />
+			))}
+
+			{!loading && messages.length === 0 && (
+				<div className='flex justify-center items-center h-full'>
+					<p className='text-gray-500'>No messages found</p>
+				</div>
+			)}
+
+		</div>
+	)
 }
 
 export default Messages
