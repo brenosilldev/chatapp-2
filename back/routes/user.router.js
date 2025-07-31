2a946a7c2ee66363f98419902872e43e7078f352
Breno Silva <brenosill@hotmail.com>
2025-07-31 01:37:20 -0300

    raw: Deletado arquivo

--- back/routes/user.router.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { GetUsersForSidebar } from '../controllers/user.controller.js';
-import { verifyToken } from '../middleware/middlware.js';
-
-const userRouter = Router();
-
-userRouter.get('/get-users-sidebar', verifyToken, GetUsersForSidebar);
-
-export default userRouter;  
