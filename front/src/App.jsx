e09d927728ece62116add0cc1685f5e9ae1a10f3
Breno Silva <brenosill@hotmail.com>
2025-07-31 01:55:29 -0300

    chore: Adicionado rotas

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,17 +1,21 @@
 import Login from "./pages/login/login"
 import Cadastro from "./pages/cadastro/cadastro"
 import Home from "./pages/home/home"
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import  { Toaster } from 'react-hot-toast';
+import { useAuthContext } from "./context/AuthContext";
+
 
 function App() {
+	const {user} = useAuthContext()
 
 	return (
 		<div className="p-4 h-screen flex items-center justify-center">
 			<Routes>
-				<Route path="/" element={<Login />} />
-				<Route path="/cadastro" element={<Cadastro />} />
-				<Route path="/home" element={<Home />} />
+				<Route path="/" element={user ? <Navigate to="/home" /> : <Login />} />
+				<Route path="/cadastro" element={user ? <Navigate to="/home" /> : <Cadastro />} />
+				<Route path="/home" element={user ? <Home /> : <Navigate to="/" />} />
+				<Route path="*" element={<Navigate to="/" />} />
 			</Routes>
 			<Toaster />
 		
