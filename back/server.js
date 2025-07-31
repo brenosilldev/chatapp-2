0a5589d8436825afc48fa26aa2560946b763ed7d
Breno Silva <brenosill@hotmail.com>
2025-07-31 02:42:02 -0300

    chore: Alterado urls

--- back/server.js
+++ back/server.js
@@ -12,10 +12,6 @@ dotenv.config();
 const PORT = process.env.PORT || 4000;
 const __dirname = path.resolve();
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
-}))
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api', MainRouter)
