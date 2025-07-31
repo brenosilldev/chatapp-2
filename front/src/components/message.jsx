d8f3b789f0d7a1e0fefaa3fe793429e807f4455c
Breno Silva <brenosill@hotmail.com>
2025-07-31 01:58:21 -0300

    chore: Alterado componente de mensagem

--- front/src/components/message.jsx
+++ front/src/components/message.jsx
@@ -1,42 +1,39 @@
 import React from 'react'
+import useConversations from '../store/useConversations'
+import { useAuthContext } from '../context/AuthContext'
 
-const Message = () => {
-  return (
-    <div>
-        <div className="chat chat-start">
+const Message = ({message, ref}) => {
+
+    const {user} = useAuthContext()
+
+    const {selectedConversation} = useConversations()
+
+    const date = new Date(message.createdAt)
+
+
+    const fromMe = message?.idUserSender?._id === user?._id || message?.idUserSender === user?._id
+
+    const image = fromMe ? user?.avatar : selectedConversation?.avatar
+
+
+    
+    return (
+    
+        <div className={`chat ${fromMe ? 'chat-end' : 'chat-start'}`} ref={ref}>
             <div className="chat-image avatar">
                 <div className="w-10 rounded-full">
                 <img
                     alt="Tailwind CSS chat bubble component"
-                    src="https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
+                    src={image}    
                 />
                 </div>
-            </div>
-            <div className="chat-header text-gray-950">
-                Obi-Wan Kenobi
-                <time className="text-xs text-gray-950">12:45</time>
-            </div>
-            <div className="chat-bubble">You were the Chosen One!</div>
-            <div className="chat-footer text-gray-950">Delivered</div>
+            </div>                
+            <div className="chat-bubble">{message.message}</div>
+            <div className="chat-footer text-gray-950">{date.toLocaleTimeString()}</div>
         </div>
-        <div className="chat chat-end">
-            <div className="chat-image avatar">
-                <div className="w-10 rounded-full">
-                <img
-                    alt="Tailwind CSS chat bubble component"
-                    src="https://img.daisyui.com/images/profile/demo/anakeen@192.webp"
-                />
-                </div>
-            </div>
-            <div className="chat-header text-gray-950">
-                Anakin
-                <time className="text-xs text-gray-950">12:46</time>
-            </div>
-            <div className="chat-bubble">I hate you!</div>
-            <div className="chat-footer text-gray-950">Seen at 12:46</div>
-        </div>
-    </div>
-  )
+           
+        
+    )
 }
 
 export default Message
