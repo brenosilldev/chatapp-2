7f1b6944782caa7ce02a4dcee4616018966f5d17
Breno Silva <brenosill@hotmail.com>
2025-07-31 01:57:15 -0300

    chore: Alterado componente de logout

--- front/src/components/logout.jsx
+++ front/src/components/logout.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { CiLogout } from 'react-icons/ci'
+import useLogout from '../hooks/useLogout'
 
 const LogoutButton = () => {
+
+    const {signOut, loading} = useLogout()
+
+    const handleLogout = async () => {
+        await signOut()
+    }
   return (
     <div className='flex items-center justify-center'>
-        <button className="btn btn-square">
+        <button className="btn btn-square" onClick={handleLogout} disabled={loading}>
             <CiLogout className='text-2xl' />
         </button>
     </div>
