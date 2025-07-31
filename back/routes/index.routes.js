ca6471ed596c24ff160ac90e4ae683c447277aba
Breno Silva <brenosill@hotmail.com>
2025-07-31 01:39:19 -0300

    chore: Alterado nome de arquivo.

--- back/routes/index.routes.js
+++ back/routes/index.routes.js
@@ -1,7 +1,7 @@
 import {Router} from "express";
 import AuthRouter from "./auth.routes.js";
 import MessageRouter from "./message.routes.js";
-import UserRouter from "./user.router.js";
+import UserRouter from "./user.routes.js";
 
 const MainRouter = Router();
 
